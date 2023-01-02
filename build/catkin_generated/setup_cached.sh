34af0b341fe2bb386a8c7eb5cec6c64652b22b67
gil <rlfghks527@naver.com>
2023-01-02 21:09:42 +0900

    init

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/gil/sparoLab/Open_Source/SLAM/LiDAR/src/A-LOAM/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/usr/local/cuda-11.1/lib64:/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/lib'
+export PATH='/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/noetic/bin:/home/gil/.local/bin:/usr/local/cuda-11.1/bin:/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin:/home/gil/.local/bin'
+export PWD='/home/gil/sparoLab/Open_Source/SLAM/LiDAR/src/A-LOAM/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/gil/sparoLab/Open_Source/SLAM/LiDAR/src/A-LOAM/build/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/gil/sparoLab/Open_Source/SLAM/LiDAR/src/A-LOAM:$ROS_PACKAGE_PATH"
